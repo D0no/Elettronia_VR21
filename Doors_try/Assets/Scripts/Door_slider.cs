47962ab07ec7f7caa4bb749db83f6a86fde4655c
LucaDonalisio <donalisioluca11@gmail.com>
2020-12-12 11:38:20 +0100

    first commit

---
+++ Doors_try/Assets/Scripts/Door_slider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Door_slider : MonoBehaviour
+{
+    private Animator _animatorL, _animatorR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animatorL = GetComponent<Animator>();
+        _animatorR = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Slider();
+    }
+    public void Slider()
+    {
+        if (_animatorL == null || _animatorR == null)
+            return;
+        _animatorL.SetBool("Slide", true);
+        _animatorR.SetBool("Slide", true);
+       
+    }
+
+}
