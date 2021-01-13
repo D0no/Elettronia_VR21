cdb682b1e5a408d4646f260f28695c112e325c59
LucaDonalisio <donalisioluca11@gmail.com>
2021-01-13 18:13:11 +0100

    update puzzle

--- Doors_try/Assets/Scripts/PhysicsGrabbable.cs
+++ Doors_try/Assets/Scripts/PhysicsGrabbable.cs
@@ -9,30 +9,36 @@ public class PhysicsGrabbable : Grabbable
     public static bool IsGrabbed = false;
     private Rigidbody _rigidbody;
     private Collider _collider;
+    private GameObject _gameobject;
+    Grabbable grabbed_object;
+
 
     protected override void Start()
     {
         base.Start();
         _collider = GetComponent<Collider>();
         _rigidbody = GetComponent<Rigidbody>();
+       _gameobject = GetComponent<GameObject>();
 
     }
 
     public override void Grab(GameObject grabber)
     {
-            IsGrabbed = true;
-            Snapper.IsSnapped = false;
-            _collider.enabled = false;
 
-            _rigidbody.isKinematic = true; //permette movimento libero
+        IsGrabbed = true;
+        Snapper.IsSnapped_static = false;
+        _collider.enabled = false;
+        _rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        _rigidbody.isKinematic = true; //permette movimento libero
         
     }
 
     public override void Drop()
     {
 
-             IsGrabbed = false;
-            _collider.enabled = true;
+        IsGrabbed = false;
+        Snapper.IsSnapped_static = false;
+        _collider.enabled = true;
         _rigidbody.constraints = RigidbodyConstraints.None;
         _rigidbody.isKinematic = false; //lo fa tornare soggetto alla gravità
         
--- Doors_try/Assets/Scripts/Snapper.cs
+++ Doors_try/Assets/Scripts/Snapper.cs
@@ -6,7 +6,8 @@ public class Snapper : MonoBehaviour
 {
     public Rigidbody _rigidbody;
     public AudioSource _snap_sound;
-    public static bool IsSnapped = false;
+    public static bool IsSnapped_static = false;
+    public bool IsSnapped = false;
     GameObject snapparent; // the gameobject this transform will be snapped to
   
     // Start is called before the first frame update
@@ -15,38 +16,42 @@ public class Snapper : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (IsSnapped == true && PhysicsGrabbable.IsGrabbed == false)
+        IsSnapped = IsSnapped_static;
+    }
+
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.tag == "wireblock" || col.tag == "wireblockrot")
+        {
+
+            IsSnapped_static = true;
+            snapparent = col.gameObject; //the collision determines where the component is snapped          
+            _rigidbody.isKinematic = false; //lo fa tornare soggetto alla gravità
+        }
+
+        if (IsSnapped_static == true && PhysicsGrabbable.IsGrabbed == false)
         {
             _rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
-
+            
             if (snapparent.tag == "wireblock") //snap the component to the wire with the transform pointing outwards
             {
+                _snap_sound.Play(); //plays snap sound
                 transform.position = snapparent.transform.position;
                 transform.rotation = snapparent.transform.rotation;
             }
 
             if (snapparent.tag == "wireblockrot") //snap the component to the wire with the transform poiting inwards
             {
+                _snap_sound.Play(); //plays snap sound
                 transform.position = snapparent.transform.position;
                 transform.rotation = Quaternion.Euler(snapparent.transform.rotation.x + 90, snapparent.transform.rotation.y, snapparent.transform.rotation.z + 180);
             }
         }
-    }
-    void OnTriggerEnter(Collider col)
-    {
-        _snap_sound.Play(); //play snap sound
 
-        if (col.tag == "wireblock" || col.tag == "wireblockrot")
-        {
-            
-            IsSnapped = true;
-            snapparent = col.gameObject; //the collision determines where the component is snapped
-            
-            _rigidbody.isKinematic = false; //lo fa tornare soggetto alla gravità
-        }
     }
   
 }
