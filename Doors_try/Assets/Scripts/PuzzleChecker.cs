5b5cab937abf3b271a5df64e1811e41d4a1f3168
LucaDonalisio <donalisioluca11@gmail.com>
2021-01-16 10:58:56 +0100

    puzzle update fixed

---
+++ Doors_try/Assets/Scripts/PuzzleChecker.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleChecker : MonoBehaviour
+{
+    public bool correct_power;
+    public bool debugged;
+    public static bool solved;
+    
+    RaycastHit hit;
+    public Vector3 _center;
+    public Vector3 _direction;
+    public float _range;
+    
+    GameObject correct_res;
+    GameObject pointed_object;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _center = gameObject.transform.position;
+       _direction = gameObject.transform.right;
+        _range = 100;
+        correct_res = GameObject.FindWithTag("res_small");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        correct_power = GameObject.FindWithTag("generator").GetComponent<Generator>().Power;
+        Ray ray = new Ray(_center, _direction);
+        Debug.DrawRay(_center, _direction, Color.green);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, _range))
+        {
+            pointed_object = hit.transform.gameObject;
+            if ((pointed_object == correct_res) && (correct_power == true) && debugged == false)
+            {
+                Debug.Log("CORRETTO");
+                debugged = true;
+                solved = true;
+            }
+        }
+    }
+}
+
